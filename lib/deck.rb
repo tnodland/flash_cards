d0c08ad056dee0429d84ad0ddfb8cd4f13870b58
Trevor Nodland <trevor.nodland@gmail.com>
2018-11-28 16:01:08 -0700

    add attr_reader and initialized

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,13 @@
 require 'pry'
 require './lib/card'
 require './lib/turn'
+
 class Deck
+  attr_reader :card, :deck
+
+  def initialize(card, deck)
+    @card = card
+    @deck = []
+      end
+      
 end
