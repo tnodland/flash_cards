7969555153f7bb64a1f4ebda925a8b26c6d4d5a5
Trevor Nodland <trevor.nodland@gmail.com>
2018-12-02 13:01:20 -0700

    created a deck in the runner file

---
+++ lib/runner.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+
+card_1 = Card.new("What stat does jolly nature raise?", "speed", :pokemon)
+card_2 = Card.new("What stat does adamant nature raise?", "attack", :pokemon)
+card_3 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_4 = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+cards = [card_1, card_2, card_3, card_4]
+deck = Deck.new(cards)
